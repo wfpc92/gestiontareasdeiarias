7fbf47aa981ace3fde2132762c8160e31a9bc431
karenj2105 <karenj2105@bc6fa086-fe4b-bbae-39a4-0d80cbafaf9e>
2014-10-22 12:54:17 +0000

    correccion estilos de los botones de pause y play, y la duracion

--- trunk/Project/js/proyecto.js
+++ trunk/Project/js/proyecto.js
@@ -2,33 +2,4 @@ $(document).ready(function() {
     $("#content-princ-der").hide()
             .css("width", "0%");
     $("#content-princ-izq").css("width", "100%");
-
-    /*$("#categoria-list-view .items .view a.menu-categoria").click(function() {
-     $(this).siblings("ul").eq(0).slideToggle();
-     return false;
-     });
-     
-     $(".actividadesPorCate .list-view .items .view a.menu-actividad").click(function() {
-     $(this).siblings().eq(1).slideToggle();
-     return false;
-     });
-     
-     $("#content-princ-izq .list-view .items .view a.menu-tarea").click(function() {
-     alert("Hola soy menu tarea");
-     return false;
-    
-    $("ul.categoria li").click(function() {
-        $("ul.categoria").hide();  
-        return false;
-     });
-     
-     $("ul.actividad li").click(function() {
-        $("ul.actividad").hide();  
-        return false;
-     });
-    
-     });*/
-    
-    
-
 });
