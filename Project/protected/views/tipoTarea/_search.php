4b86e53fb25b15921cf5f3c4c638d79bb430320e
wfpantoja <wfpantoja@bc6fa086-fe4b-bbae-39a4-0d80cbafaf9e>
2014-12-12 04:48:06 +0000

    Tipo de tarea acomodando desde la bd

---
+++ Project/protected/views/tipoTarea/_search.php
@@ -0,0 +1,35 @@
+<?php
+/* @var $this TipoTareaController */
+/* @var $model TipoTarea */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id_tipo_tarea'); ?>
+		<?php echo $form->textField($model,'id_tipo_tarea',array('size'=>10,'maxlength'=>10)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'nombre'); ?>
+		<?php echo $form->textField($model,'nombre',array('size'=>45,'maxlength'=>45)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'id_usuario'); ?>
+		<?php echo $form->textField($model,'id_usuario',array('size'=>10,'maxlength'=>10)); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
